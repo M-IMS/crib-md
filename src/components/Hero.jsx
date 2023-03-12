6642f41dafb41736a5b7372938a159b24c8519ba
Imtiaz Mahmud <70899905+M-IMS@users.noreply.github.com>
2023-03-12 19:18:42 +0600

    hero some fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
+import hero from "../assets/Hero.svg";
+
 const Hero = () => {
   return (
-    <div>
-      <div>
-        <h1>Our Biggest Sales Yet...</h1>
-        <p>
+    <div className="relative border-2 border-red-400">
+      <img src={hero} alt="" className="absolute" />
+      <div className="relative w-full h-auto sm:mt-40 p-5 space-y-8 text-center ">
+        <h1 className="text-4xl font-bold">Our Biggest Sales Yet...</h1>
+        <p className="text-lg font-semibold text-gray-800">
           We are offering massive discounts on all our health plans during our
           Black Friday and Cyber Monday sales.
         </p>
-        <button>Get Started</button>
+        <button className="px-4 py-3 bg-blue-500 font-semibold text-white hover:text-black rounded-lg cursor-pointer">
+          Get Started
+        </button>
       </div>
     </div>
   );
